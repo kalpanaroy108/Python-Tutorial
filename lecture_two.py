0585eb3d75370c40938f924aa2858e234964be15
kalpanaroy <kalpanaroy832@gmail.com>
2025-05-17 21:38:08 +0530

    first commit

---
+++ lecture_two.py
@@ -0,0 +1,14 @@
+#print("Hello world")
+# str1 = "This is a string."
+# str2 = 'Apnacollage'
+# str3 = """this is a string"""
+
+# "this is apnacollage's toturial"
+# 'this is apnacollage"s toturial'
+
+# str1 = "this is a string. \nwe are creating it in python."
+# print(str1)
+# str1 = "this is a string. we are creating it in python"
+# print (str1)
+str1 = "this is a string. \twe are creating it in python"
+print (str1)
